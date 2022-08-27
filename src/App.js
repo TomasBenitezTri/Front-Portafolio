20242510622b92b64c88523cf33679ad950b5981
Tomas Benitez <tomasbenitezc13@gmail.com>
2022-08-27 16:17:05 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Tomas Benitez</h1>
+      <h2>Backend Developer</h2>
+      <h3>Javascript - PHP - C# - SQL</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
